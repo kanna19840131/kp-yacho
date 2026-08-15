1ecd99634a558bd69aebbd7565a73bac0019f0f7
kanna19840131 <kanna19840131@gmail.com>
2026-08-16 02:49:33 +0900

    Add beta runtime config

---
+++ beta-route-config.js
@@ -0,0 +1,11 @@
+window.KP_YACHO_BETA_CONFIG={
+  schemaVersion:1,
+  productId:'beta-template',
+  productName:'KP野帳 β',
+  providerLabel:'configured by 石川義晴',
+  warnDistanceM:30,
+  maxDistanceM:200,
+  kpDecimals:2,
+  defaultRate:45,
+  routes:[]
+};
